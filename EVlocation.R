bf18ae18c58fc464390e6a49c8efaa87ea0c7b2a
Jun <majun0118@gmail.com>
2016-02-28 23:37:08 -0600

    add manhattan distance

--- EVlocation.R
+++ EVlocation.R
@@ -3,6 +3,7 @@ library(grid)
 library(dplyr)
 library(ggplot2)
 library(broom)
+library(amap)
 
 time.df <- read.csv('data/1Timedcar2go_week.csv', header = T)
 #car location plot
@@ -42,8 +43,8 @@ wss <- data.frame(clusterNo = seq(1,50), wss = rep(0, 50))
 
 for (i in 1:50){
   
-  clust.k <-time.df %>% select(Longitude, Latitude) %>% kmeans(i, iter.max=500)
-  wss$wss[i] <- clust.k$tot.withinss
+  clust.k <-time.df %>% select(Longitude, Latitude) %>% kmeans(50, iter.max=500)
+  wss$wss[i] <- sum(clust.k$withinss)
 }
 p2 <- ggplot(wss)+geom_point(aes(clusterNo, wss), size = 4, shape = 1, color='#009E73')+
   xlab('No. of Centroids') + ylab('WSS') +
@@ -71,14 +72,6 @@ p3.2<- ggmap(get_map('domain, austin', zoom = 15), extent = 'device')+
         panel.border = element_rect(colour = "black", fill = NA, size=2))
 
 plot_inset('3.png', p3, p3.2)
-
-# png('3.png', width=1280, height=1280)
-# grid.newpage()
-# v1<-viewport(width = 1, height = 1, x = 0.5, y = 0.5) #plot area for the main map
-# v2<-viewport(width = 0.2, height = 0.2, x = 0.18, y = 0.83) #plot area for the inset map
-# print(p3,vp=v1) 
-# print(p3.2,vp=v2)
-# dev.off() 
 
 station.df <- read.csv('data/charging_stations (Feb 20 2016).csv', header = T)
 station.austin = station.df%>%dplyr::filter(City=='Austin')
@@ -139,10 +132,22 @@ plot_inset('5.png', p5, p5.2)
 a <- station.dist %>%
   filter(distToExist>0.5) %>%
   summarise(count=n())
-# png('5.png', width=1280, height=1280)
-# grid.newpage()
-# v1<-viewport(width = 1, height = 1, x = 0.5, y = 0.5) #plot area for the main map
-# v2<-viewport(width = 0.2, height = 0.2, x = 0.18, y = 0.83) #plot area for the inset map
-# print(p5,vp=v1) 
-# print(p5.2,vp=v2)
-# dev.off() 
+
+# manhattan distance
+clust <-time.df %>% select(Longitude, Latitude) %>% kmeans(50, iter.max=500, method="manhattan")
+p3<- ggmap(get_map(location = 'austin', zoom = 12), extent = 'device')+
+  geom_point(data=augment(clust, time.df),
+             aes(x = Longitude, y = Latitude, color = .cluster), alpha =0.1, size = 4) +
+  geom_point(aes(Longitude, Latitude), data = data.frame(clust$centers), size = 15, shape = 'x') + 
+  theme(legend.position = 'none')
+
+p3.2<- ggmap(get_map('domain, austin', zoom = 15), extent = 'device')+
+  geom_point(data=augment(clust, time.df),
+             aes(x = Longitude, y = Latitude, color = .cluster), alpha =0.1, size = 4) +
+  geom_point(aes(Longitude, Latitude), data = data.frame(clust$centers), size = 15, shape = 'x') + 
+  ggtitle('The Domain')+
+  theme(legend.position = 'none', 
+        plot.title = element_text(size = rel(2)),
+        panel.border = element_rect(colour = "black", fill = NA, size=2))
+
+plot_inset('6.png', p3, p3.2)
